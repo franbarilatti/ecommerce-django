e731905a88b3544fa3bdd81282d66313d9aa7e9c
franco <francoagustinbarilatti@hotmail.com>
2025-01-20 21:46:22 -0300

    se agrego logica para almacenar variations en el cartitem

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, Page, PageNotAnInteger, Paginator
+from django.db.models import Q
 
 
 # Create your views here.
@@ -14,13 +15,13 @@ def store(request, category_slug=None):
     
     if category_slug != None:
         categories = get_object_or_404(Category, slug=category_slug)
-        products = Product.objects.filter(category=categories, is_available=True)
+        products = Product.objects.filter(category=categories, is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(is_available=True).order_by('id')
         paginator = Paginator(products, 6)
         page = request.GET.get('page')
         paged_products = paginator.get_page(page)
@@ -50,3 +51,23 @@ def product_detail(request, category_slug, product_slug):
     
     
     return render(request, 'store/product_detail.html', context)
+
+
+def search(request):
+    print("Search function called")
+    
+    products = Product.objects.all()  # Inicializamos con un queryset vacío
+    product_count = 0  # Inicializamos el conteo en 0
+    
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-create_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword))
+            product_count = products.count()
+            
+    context={
+        'products': products,
+        'product_count': product_count,
+    }
+    
+    return render(request, 'store/store.html')
